a32491e968e578b07963c8a7b8429242513d63c4
Ryan Hansen <rphansen91@gmail.com>
2017-01-19 21:24:14 -0800

    Use promise as callback

--- device-display.js
+++ device-display.js
@@ -43,25 +43,12 @@ const loadDevice = require('./device');
 const render = require('./render');
 const insert = require('./insert');
 
-const isFn = fn => x => {
-    if (typeof fn === 'function') return fn(x);
-    return x;
-}
-
 window.DeviceDisplay = (frame, opts) => {
-    let ls = [];
     if (!opts) return new Error('Must supply options');
     
-    loadDevice(opts)
+    return loadDevice(opts)
     .then(render(opts))
     .then(insert(frame))
-    .then(d => isFn(ls[0])(d))
-    .catch(e => isFn(ls[1])(e));
-
-    return {
-        ready: l => ls[0] = l,
-        error: e => ls[1] = e
-    }
 }
 },{"./device":2,"./insert":3,"./render":6}],5:[function(require,module,exports){
 Element.prototype.css = function (style) {
--- src/main.js
+++ src/main.js
@@ -2,23 +2,10 @@ const loadDevice = require('./device');
 const render = require('./render');
 const insert = require('./insert');
 
-const isFn = fn => x => {
-    if (typeof fn === 'function') return fn(x);
-    return x;
-}
-
 window.DeviceDisplay = (frame, opts) => {
-    let ls = [];
     if (!opts) return new Error('Must supply options');
     
-    loadDevice(opts)
+    return loadDevice(opts)
     .then(render(opts))
     .then(insert(frame))
-    .then(d => isFn(ls[0])(d))
-    .catch(e => isFn(ls[1])(e));
-
-    return {
-        ready: l => ls[0] = l,
-        error: e => ls[1] = e
-    }
 }
